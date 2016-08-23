7f34c93bacca9804da0811f702c2538dca2c6989
musaunal <musaunal680@gmail.com>
2016-08-23 21:44:55 +0300

    lesson 10 compleated
    -obj file loader class added

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -10,6 +10,7 @@ import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -24,97 +25,20 @@ public class MainGameLoop {
 		Renderer renderer = new Renderer(shader);
 	
 		
-		float[] vertices = { 
-				-0.5f,0.5f,0,	
-				-0.5f,-0.5f,0,	
-				0.5f,-0.5f,0,	
-				0.5f,0.5f,0,		
-				
-				-0.5f,0.5f,1,	
-				-0.5f,-0.5f,1,	
-				0.5f,-0.5f,1,	
-				0.5f,0.5f,1,
-				
-				0.5f,0.5f,0,	
-				0.5f,-0.5f,0,	
-				0.5f,-0.5f,1,	
-				0.5f,0.5f,1,
-				
-				-0.5f,0.5f,0,	
-				-0.5f,-0.5f,0,	
-				-0.5f,-0.5f,1,	
-				-0.5f,0.5f,1,
-				
-				-0.5f,0.5f,1,
-				-0.5f,0.5f,0,
-				0.5f,0.5f,0,
-				0.5f,0.5f,1,
-				
-				-0.5f,-0.5f,1,
-				-0.5f,-0.5f,0,
-				0.5f,-0.5f,0,
-				0.5f,-0.5f,1
-				
-		};
+	
 		
-		int[] indices = {
-				0,1,3,	
-				3,1,2,	
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,	
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
-		};
+		RawModel model = OBJLoader.loadObjModel("stall", loader); 
 		
-		float[] textureCoords = {
-
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
-		};
-
-		
-		RawModel model = loader.loadToVAO(vertices ,textureCoords, indices);
-		
-		//ModelTexture texture = new ModelTexture(loader.loadTexture("a"));
-		
-		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("a")));
+		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
 		
 		
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1); 
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-25),0,0,0,1); 
 		
 		Camera camera = new Camera();
 		
 		while (!Display.isCloseRequested()){
 			//entity.increasePosition(0, 0, -0.1f);
-			entity.increaseRotation(1, 1, 0);
+			entity.increaseRotation(0, 1, 0);
 			camera.move();
 			renderer.prepare();
 			shader.start();
